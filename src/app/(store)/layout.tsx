8d407437576495ad3e07e055e8454e70db46110b
Rafael Lodi <rafael.lodi@gbmlogistica.com.br>
2023-12-26 19:31:00 -0300

    Componente: Header

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+import { Header } from '@/components/header'
+
+export default function HomeLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
+      <Header />
+
+      {children}
+    </div>
+  )
+}
